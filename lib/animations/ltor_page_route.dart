98857e49bb342b587b396085b3d8268d1b9fb8d6
190107062@stu.sdu.edu.kz <190107062@stu.sdu.edu.kz>
2021-11-16 23:32:58 +0600

    Left to Right Routing Animation

---
+++ lib/animations/ltor_page_route.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+class LtorPageRoute extends PageRouteBuilder {
+  final Widget child;
+
+  LtorPageRoute({
+    required this.child,
+  }) : super(
+    transitionDuration: Duration(milliseconds: 300),
+    pageBuilder: (context, animation, secondaryAnimation) => child,
+  );
+
+  @override
+  Widget buildTransitions(BuildContext context, Animation<double> animation,
+      Animation<double> secondaryAnimation, Widget child) =>
+      SlideTransition(
+        position: Tween<Offset>(
+          begin: Offset(-1,0),
+          end: Offset.zero,
+        ).animate(animation),
+        child: child,
+      );
+}
