d3fcf67fa25d4532bd0f275bc30aea539101e66e
190107062@stu.sdu.edu.kz <190107062@stu.sdu.edu.kz>
2021-11-16 22:33:48 +0600

    Folder animations is created

---
+++ lib/animations/custom_page_route.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class CustomPageRoute extends PageRouteBuilder {
+  final Widget child;
+
+  CustomPageRoute({
+    required this.child,
+}) : super(
+      transitionDuration: Duration(milliseconds: 500),
+      pageBuilder: (context, animation, secondaryAnimation) => child,
+  );
+
+  @override
+  Widget buildTransitions(BuildContext context, Animation<double> animation,
+      Animation<double> secondaryAnimation, Widget child) =>
+      ScaleTransition(
+        scale: animation,
+        child: child,
+      );
+}
